90b0733e5cefa8f94b8afb5f8d82e2fe2f89df4c
Alaa Khalil <alaakhalil093@gmail.com>
2021-12-31 19:35:23 +0100

    Complete LocationsViewController

---
+++ Locations/Locations/Cells/LocationsCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationsTableViewCell.swift
+//  Locations
+//
+//  Created by Alaa Khalil on 23/12/2021.
+//
+
+import UIKit
+
+class LocationsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var LocationNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Locations/Locations/Controllers/LocationsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LocationsViewController.swift
+//  Locations
+//
+//  Created by Alaa Khalil on 23/12/2021.
+//
+
+import UIKit
+
+class LocationsViewController: UIViewController {
+
+    @IBOutlet weak var locationTableView: UITableView!
+    
+    var locations = [Location]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.getLocations()
+        // Do any additional setup after loading the view.
+    }
+
+
+    // Fetching the data from Api
+    func getLocations(){
+        ServerManger.shared.getHouses{ response, error in
+            if error == nil{
+                self.locations = response.locations!
+            }
+            self.locationTableView.reloadData()
+        }
+    }
+}
+
+extension LocationsViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.locations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = locationTableView.dequeueReusableCell(withIdentifier: "LocationCell") as! LocationsTableViewCell
+        let location = self.locations[indexPath.row]
+        cell.LocationNameLabel.text = location.name ?? "Unknwon"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.OpenWikipedia(location: self.locations[indexPath.row])
+    }
+    
+    func OpenWikipedia(location: Location){
+        let wikiUrlString = String(format: wikiDeepLink, location.name ?? " ", location.lat, location.long)
+        let wikiUrl = URL(string: wikiUrlString)! as URL
+        print(wikiUrl)
+        if (UIApplication.shared.canOpenURL(wikiUrl)) {
+            UIApplication.shared.open(wikiUrl)
+        }
+    }
+}
+
