7540237f2215db60bfcc66528052ba1e3dac41b6
Alaa Khalil <alaakhalil093@gmail.com>
2021-12-31 19:36:08 +0100

    Constants

---
+++ Locations/Locations/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Locations
+//
+//  Created by Alaa Khalil on 31/12/2021.
+//
+
+import Foundation
+
+
+let locationsUrl = "https://raw.githubusercontent.com/abnamrocoesd/assignment-ios/main/locations.json"
+
+let wikiBaseUrl = "wikipedia://places/"
+let wikiLocationUrl = wikiBaseUrl + "location/"
+let wikiDeepLink = wikiLocationUrl + "?name=%@&latitude=%f&longitude=%f"
