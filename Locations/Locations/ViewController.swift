c0b5158f365b193ea88cc96b9d4c489acb921912
Alaa Khalil <alaakhalil093@gmail.com>
2021-12-31 19:30:22 +0100

    Initial commit

---
+++ Locations/Locations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locations
+//
+//  Created by Alaa Khalil on 23/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
